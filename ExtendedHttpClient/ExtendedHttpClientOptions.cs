5af9ab7178fdd309b884d0642673f4e09929e9e2
miha <miha890r@gmail.com>
2022-10-26 20:05:48 +0600

    AddProjectFiles

---
+++ ExtendedHttpClient/ExtendedHttpClientOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtendedHttpClient;
+
+public class ExtendedHttpClientOptions<T>
+{
+    public string Url { get; private set; }
+
+    public ExtendedHttpClientOptions(string url)
+    {
+        Url = url;
+    }
+}
---
+++ ExtendedHttpClient/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using ExtendedHttpClient;
+using ExtendedHttpClient.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExtendedHttpClient.Extensions;
+
+public static class Extensions
+{
+    public static IServiceCollection AddServiceWithExtendedHttpClient<TClass, TImplementation>(this IServiceCollection services, string uri)
+        where TClass : class, IUseExtendedHttpClient<TClass>
+        where TImplementation : class, TClass
+    {
+        services.AddSingleton(provider => new ExtendedHttpClientOptions<TClass>(uri));
+        services.AddTransient<ExtendedHttpClient<TClass>>();
+        services.AddTransient<TClass, TImplementation>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddServiceWithExtendedHttpClient<TClass>(this IServiceCollection services, string uri)
+        where TClass : class, IUseExtendedHttpClient<TClass>
+    {
+        services.AddSingleton(provider => new ExtendedHttpClientOptions<TClass>(uri));
+        services.AddTransient<ExtendedHttpClient<TClass>>();
+        services.AddTransient<TClass>();
+
+        return services;
+    }
+}
